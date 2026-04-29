65dd50bc737073c06991f591d65bd21412a5934a
Lance <1960595@gmail.com>
2026-04-29 16:49:25 +0800

    修复空 start 参数导致的启动失败

--- src/Modules/TelegramPanel.Module.BotCheckinAssistant/BotCheckinAssistantModule.cs
+++ src/Modules/TelegramPanel.Module.BotCheckinAssistant/BotCheckinAssistantModule.cs
@@ -12,7 +12,7 @@ public sealed class BotCheckinAssistantModule : ITelegramPanelModule, IModuleUiP
     {
         Id = "community.bot-checkin-assistant",
         Name = "机器人签到助手",
-        Version = "1.1.0",
+        Version = "1.1.1",
         Host = new HostCompatibility
         {
             Min = "1.0.0",
